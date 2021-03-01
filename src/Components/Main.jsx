96d61befa8601b5d289eb72acd2b4584be12cbb0
Evens Pompe <evenspompe9593@gmail.com>
2021-03-01 23:48:11 +0100

    Modif Register : add form

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -5,7 +5,7 @@ import Login from "../Views/Login";
 import { Switch, Route } from "react-router-dom";
 function Main() {
     return (
-        <div id="main" className="w-screen">
+        <div id="main" className="w-screen bg-pink-200">
             <Switch>
                 <Route exact path="/">
                     <Home/>
--- src/Views/Register.jsx
+++ src/Views/Register.jsx
@@ -1,5 +1,53 @@
-export default function Register(){
-    return(
-        <h1>Register</h1>
+import React,{useState} from "react";
+export default function Register() {
+    const [email,setEmail] = useState(""),
+          [password,setPassword] = useState(""),
+          [confPassword,setConfPassword] = useState("");
+
+    const sendRegister = (e) =>{
+        e.preventDefault();
+        if(password === confPassword){
+            console.log("ok")
+        }else{
+            console.log("not ok")
+        }
+        console.log(email,password,confPassword)
+    }
+    return (
+        <div id="register" className="w-screen h-full flex flex-col flex-nowrap">
+            <div className="text-center h-1/4 flex justify-center items-center">
+                <h1 className="text-6xl">Inscription</h1>
+            </div>
+            <form id="formRegister" className="h-3/4" onSubmit={sendRegister}>
+                <div className="h-1/4 w-screen">
+                    <div className="h-1/2">
+                        <label htmlFor="email" className="text-2xl">Email</label>
+                    </div>
+                    <div className="h-1/2">
+                        <input className="w-80 focus:ring-2 focus:ring-black" onChange={(e)=> setEmail(e.target.value)} type="email" name="email" id="email" required/>
+                    </div>
+                </div>
+                <div className="h-1/4 w-screen">
+                    <div className="h-1/2">
+                        <label htmlFor="password" className="text-2xl">Mot de passe</label>
+                    </div>
+                    <div className="h-1/2">
+                        <input className="w-80 focus:ring-2 focus:ring-black" onChange={(e)=> setPassword(e.target.value)} type="password" name="password" id="password" required/>
+                    </div>
+                </div>
+                <div className="h-1/4 w-screen">
+                    <div className="h-1/2">
+                        <label htmlFor="confPassword" className="text-2xl">Confirmer votre mot de passe</label>
+                    </div>
+                    <div className="h-1/2">
+                        <input className="w-80 focus:ring-2 focus:ring-black" onChange={(e)=> setConfPassword(e.target.value)} type="password" name="confPassword" id="confPassword" required/>
+                    </div>
+                </div>
+                <div className="h-1/4 w-screen">
+                    <button type="submit" className="text-2xl bg-pink-500 border-2 border-black w-40 rounded-md focus:ring-2 
+                    focus:ring-black hover:bg-gray-600 active:bg-gray-200">S'inscrire</button>
+                </div>
+            </form>
+        </div>
     );
 }
